cf8abf68e8e144b7abfe6e1eb192be0d381c52b5
Lubomir I. Ivanov <neolit123@gmail.com>
2015-03-18 02:30:34 +0200

    add means to automatically detect the ADL version
    - writedesc.cmd is now used to write a descriptor
    in the ./bin/ folder
    - build.cmd can now detect the ADL version and pass that
    to writedesc.cmd
    
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,2 +1,40 @@
 @echo off
-mxmlc +configname=air ./src/Main.as -output ./bin/ta-gen.swf -library-path+=./lib
+setlocal EnableDelayedExpansion
+
+::
+:: version to be written to the descriptor XML
+::
+set VERSION_TAGEN=1.3
+::
+
+:: extract the ADL version from the command line
+set VERSION_ADL=
+set TMPFILE=.\adl.tmp
+set /a c=0
+
+adl 2> nul > !TMPFILE!
+for /f "tokens=*" %%a in (%TMPFILE%) do (
+	if !c! equ 1 (
+		set VERSION_ADL=%%a & goto found_version
+	)
+	set /a c=c+1
+)
+goto error
+
+:found_version
+set VERSION_ADL=%VERSION_ADL:~8,4%
+echo found ADL version: %VERSION_ADL%
+cmd /c writedesc %VERSION_ADL% %VERSION_TAGEN%
+echo building...
+cmd /c mxmlc +configname=air ./src/Main.as -output ./bin/ta-gen.swf -library-path+=./lib
+
+goto end
+
+:error
+echo ERROR: cannot find ADL or obtain it's version
+
+:end
+del /q %TMPFILE%
+set TMPFILE=
+set VERSION_ADL=
+set VERSION_TAGEN=
---
+++ writedesc.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+set FILE=.\bin\ta-gen.xml
+set VERSION_ADL=%1
+if "%VERSION_ADL%"=="" echo defaulting VERSION_ADL to 17.0 & set VERSION_ADL=17.0
+set VERSION_TAGEN=%2
+if "%VERSION_TAGEN%"=="" echo defaulting VERSION_TAGEN to 1.0 & set VERSION_ADL=1.0
+
+echo writing descriptor for ADL %VERSION_ADL%, ta-gen %VERSION_TAGEN%...
+
+(
+	echo ^<?xml version="1.0" encoding="UTF-8" standalone="no" ?^>
+	echo ^<application xmlns="http://ns.adobe.com/air/application/%VERSION_ADL%"^>
+	echo 	^<id^>ta-gen^</id^>
+	echo 	^<versionNumber^>%VERSION_TAGEN%^</versionNumber^>
+	echo 	^<filename^>ta-gen^</filename^>
+	echo 	^<description/^>
+	echo 	^<name^>ta-gen^</name^>
+	echo 	^<copyright/^>
+	echo 	^<initialWindow^>
+	echo 		^<content^>ta-gen.swf^</content^>
+	echo 		^<systemChrome^>standard^</systemChrome^>
+	echo 		^<transparent^>false^</transparent^>
+	echo 		^<visible^>false^</visible^>
+	echo 		^<fullScreen^>false^</fullScreen^>
+	echo 		^<aspectRatio^>portrait^</aspectRatio^>
+	echo 		^<renderMode^>auto^</renderMode^>
+	echo 	^</initialWindow^>
+	echo 	^<icon/^>
+	echo 	^<customUpdateUI^>false^</customUpdateUI^>
+	echo 	^<allowBrowserInvocation^>false^</allowBrowserInvocation^>
+	echo ^</application^>
+) > %FILE%
+
+set VERSION_TAGEN=
+set VERSION_ADL=
+set FILE=
