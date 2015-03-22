1bd6bcdae1d5a55db66aacc911f2480a83a25d2c
Lubomir I. Ivanov <neolit123@gmail.com>
2015-03-22 23:19:53 +0200

    update test.cmd/sh
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- test/test.cmd
+++ test/test.cmd
@@ -1,2 +1,2 @@
 @echo off
-../bin/ta-gen -verbose -poweroftwo -in ./input -out ./output/test.png -pngprefix pngprefix/ -subprefix subprefix/ -mindim 32 -maxdim 1024 -dither -colorbits 4 -background 0xFF00FF00 -padding 4 -extrude 4 -pngencoder 2 -ignore ./input/part1/0.png
+../bin/ta-gen -verbose -poweroftwo -in ./input -out ./output/test.png -pngprefix pngprefix/ -subprefix subprefix/ -mindim 32 -maxdim 1024 -colorbits 4 -background 0xFF00FF00 -padding 4 -extrude 4 -pngencoder 2 -ignore ./input/part1/0.png
