f3a5e623e9887263c2684903ad0ea0fab5ac91d1
Lubomir I. Ivanov <neolit123@gmail.com>
2015-03-27 18:36:13 +0200

    modify build.cmd to read the version from VERSION
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,7 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set VERSION_TAGEN=1.5
+set VERSION_FILE=.\VERSION
+if not exist %VERSION_FILE% goto error_versio_file
+set /p VERSION_TAGEN=<%VERSION_FILE%
+echo found VERSION file: %VERSION_TAGEN%
+
 set BUILD_COMMAND=%AIR_SDK_BIN%mxmlc +configname=air ./src/Main.as -output ./bin/ta-gen.swf -library-path+=./lib %*
 
 :: extract the ADL version from the command line
@@ -12,13 +16,13 @@ set /a c=0
 %AIR_SDK_BIN%adl 2> NUL > !TMPFILE!
 for /f "tokens=*" %%a in (%TMPFILE%) do (
 	if !c! equ 1 (
-		set VERSION_ADL=%%a & goto found_version
+		set VERSION_ADL=%%a & goto found_adl_version
 	)
 	set /a c=c+1
 )
-goto error
+goto error_adl
 
-:found_version
+:found_adl_version
 set VERSION_ADL=%VERSION_ADL:~8,4%
 echo found ADL version: %VERSION_ADL%
 cmd /c writedesc %VERSION_ADL% %VERSION_TAGEN%
@@ -28,8 +32,13 @@ cmd /c %BUILD_COMMAND%
 
 goto end
 
-:error
+:error_adl
 echo ERROR: cannot find ADL or obtain it's version
+goto end
+
+:error_version_file
+echo ERROR: cannot find version file
+goto end
 
 :end
 del /q %TMPFILE%
